e683375f819d89ac765ed56b610264440b4e515f
asd89567 <as89567123@gmail.com>
2023-05-03 20:42:08 +0800

    411004265

---
+++ Web_411004265/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_411004265.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Bocky
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HW1()
+        {
+            return View();
+        }
+    }
+}
